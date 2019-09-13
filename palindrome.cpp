87bc2a3aa4a314fe0348a119ae4b4849ea25f043
U-BSD\370801 <370801@SHS-10DECBC5A.beaverton.k12.or.us>
2019-09-13 09:11:16 -0700

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -5,7 +5,12 @@ using namespace std;
 
 int main() {
 	char input[80];
-	cin.get(input, 80); 
-	cout << input << endl;
+	char flip[80];
+	cin.get(input, 80);
+	int length = strlen(input);
+        for (int i = 0; i < length; i++) {
+		flip[i] = input[length - i];	
+	}	
+	cout << flip << endl;
 	return 0;
 }
