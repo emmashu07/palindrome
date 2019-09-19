fbf5471c9d0b8bd7216cc0877dd89f6a2a4eddd8
U-BSD\370801 <370801@SHS-10DECBC5A.beaverton.k12.or.us>
2019-09-19 09:13:35 -0700

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -8,21 +8,27 @@ int main() {
 	char input[80];
 	char flip[80];
 	char origin[80];
-	int num = 0;
+	int num = 0;	
 	cin.get(input, 80);
 	int length = strlen(input);
-	for (int i = 0; i < length; i++) {
-		if(!ispunct(input[i]) || !isspace(input[i])) {
-			origin[num] = input[i];
+	for (int i = 0; i < length; i++) {	
+		if(!ispunct(input[i]) && !isspace(input[i])) {
+			origin[num] = tolower(input[i]);
 			num++;	
 		}	
 	}
-	cout << input << endl;
-        for (int i = 0; i < length; i++) {
-		if (input[length - i] != '\0') {
-			flip[i] = input[length - i];	
-		}	
+	origin[num] = '\0';	
+        for (int i = 0; i < num; i++) {
+		if(origin[num - i - 1] != '\0') {	
+			flip[i] = origin[num - i - 1];	
+		}		
+	}
+	flip[num] = '\0';
+	if(strcmp(origin,flip) == 0) {
+		cout << "Palindrome." << endl;
+	}
+	else {
+		cout << "Not a palindrome." << endl;
 	}	
-	cout << flip << endl;
 	return 0;
 }
