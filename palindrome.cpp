bf11faf94e1cf98862f9c8d13908887ab337050e
U-BSD\370801 <370801@SHS-10DECBC5A.beaverton.k12.or.us>
2019-09-23 07:50:15 -0700

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -4,23 +4,28 @@
 
 using namespace std;
 
+/* Emma Shu: Palindrome is a program that identifies whether a word
+ * or phrase, regardless of spaces, punctuation, or upper case letters,
+ * is a palindrome or not.
+ */
+
 int main() {
 	char input[80];
 	char flip[80];
 	char origin[80];
 	int num = 0;	
 	cin.get(input, 80);
-	int length = strlen(input);
+	int length = strlen(input); //Get length of the input.
 	for (int i = 0; i < length; i++) {	
-		if(!ispunct(input[i]) && !isspace(input[i])) {
-			origin[num] = tolower(input[i]);
+		if(!ispunct(input[i]) && !isspace(input[i])) { //Do not input spaces/punctuation.
+			origin[num] = tolower(input[i]); //Make all letters lower case.
 			num++;	
 		}	
 	}
-	origin[num] = '\0';	
+	origin[num] = '\0'; //Null terminating character.	
         for (int i = 0; i < num; i++) {
 		if(origin[num - i - 1] != '\0') {	
-			flip[i] = origin[num - i - 1];	
+			flip[i] = origin[num - i - 1]; //Flip input into char array.	
 		}		
 	}
 	flip[num] = '\0';
